0e77273fb5a2514d036b60469a21abcba648c177
syuji-higa <syuji.higa@gmail.com>
2021-06-12 13:27:31 +0900

    test: add Total Counter

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -84,3 +84,86 @@ describe('カウンター A は', () => {
     })
   })
 })
+
+describe('合計カウンターは', () => {
+  // 前準備（arrange）
+  const regexpCountA = /A：(-?[0-9]+)/i
+  const regexpCountB = /B：(-?[0-9]+)/i
+  const regexpTotalCount = /合計：(-?[0-9]+)/i
+  let countAElement: HTMLElement
+  let countBElement: HTMLElement
+  let totalCountElement: HTMLElement
+  let incrementElement: HTMLElement
+  let decrementElement: HTMLElement
+  beforeEach(() => {
+    const { getByRole, getByText } = render(<App />)
+    countAElement = getByText(regexpCountA)
+    countBElement = getByText(regexpCountB)
+    totalCountElement = getByText(regexpTotalCount)
+    incrementElement = getByRole('button', { name: 'Total Counter increment' })
+    decrementElement = getByRole('button', { name: 'Total Counter decrement' })
+  })
+
+  describe('はじめに', () => {
+    it('合計カウントに 0 を表示する', () => {
+      // 検証（assert）
+      const text = totalCountElement.textContent
+      const [, count] = Array.from(text?.match(regexpTotalCount) ?? [])
+      expect(count).toBe('0')
+    })
+  })
+
+  describe('+ ボタンを 1 回クリックすると', () => {
+    it('カウント A に 1 を表示する', () => {
+      // 実行（act）
+      fireEvent.click(incrementElement)
+      const text = countAElement.textContent
+      const [, count] = Array.from(text?.match(regexpCountA) ?? [])
+      // 検証（assert）
+      expect(count).toBe('1')
+    })
+    it('カウント B に 1 を表示する', () => {
+      // 実行（act）
+      fireEvent.click(incrementElement)
+      const text = countBElement.textContent
+      const [, count] = Array.from(text?.match(regexpCountB) ?? [])
+      // 検証（assert）
+      expect(count).toBe('1')
+    })
+    it('合計カウントに 2 を表示する', () => {
+      // 実行（act） 
+      fireEvent.click(incrementElement)
+      const text = totalCountElement.textContent
+      const [, count] = Array.from(text?.match(regexpTotalCount) ?? [])
+      // 検証（assert）
+      expect(count).toBe('2')
+    })
+  })
+
+  describe('- ボタンを 1 回クリックすると', () => {
+    it('カウント A に 1 を表示する', () => {
+      // 実行（act）
+      fireEvent.click(decrementElement)
+      const text = countAElement.textContent
+      const [, count] = Array.from(text?.match(regexpCountA) ?? [])
+      // 検証（assert）
+      expect(count).toBe('-1')
+    })
+    it('カウント B に 1 を表示する', () => {
+      // 実行（act）
+      fireEvent.click(decrementElement)
+      const text = countBElement.textContent
+      const [, count] = Array.from(text?.match(regexpCountB) ?? [])
+      // 検証（assert）
+      expect(count).toBe('-1')
+    })
+    it('合計カウントに -2 を表示する', () => {
+      // 実行（act） 
+      fireEvent.click(decrementElement)
+      const text = totalCountElement.textContent
+      const [, count] = Array.from(text?.match(regexpTotalCount) ?? [])
+      // 検証（assert）
+      expect(count).toBe('-2')
+    })
+  })
+})
--- src/components/TotalCount.tsx
+++ src/components/TotalCount.tsx
@@ -10,10 +10,20 @@ type Props = {
 export const TotalCount: VFC<Props> = ({ totalCount, increment, decrement }) => {
   return (
   	<div>
-      <div>合計：{totalCount}</div>
+      <div
+        aria-label={`Total Counter count`}
+      >合計：{totalCount}</div>
       <span>両方を増減</span>
-      <button type="button" onClick={decrement}>-</button>
-      <button type="button" onClick={increment}>+</button>
+      <button
+        aria-label={`Total Counter decrement`}
+        type="button"
+        onClick={decrement}
+      >-</button>
+      <button
+        aria-label={`Total Counter increment`}
+        type="button"
+        onClick={increment}
+      >+</button>
     </div>
   )
 }
